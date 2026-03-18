238f12cde9b74f92b4ea13a1b2f72129647a1f11
Jonas I. Liechti <j-i-l@t4d.ch>
2026-03-18 22:28:51 +0100

    adding slurm script

---
+++ scripts/slurm/submit_hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=helloworld
+# Get the account with
+# sacctmgr show assoc user=<username> format=User,Account%50
+#SBATCH --account=<group.department.uni>
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --time=00:10:00
+#SBATCH --output=logs/%j.out
+
+module load apptainer
+
+apptainer run \
+  --bind ./data/raw:/app/data/raw \
+  --bind ./data/final:/app/data/final \
+  env-sif_latest.sif \
+  python scripts/say_hello.py
